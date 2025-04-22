63b10683c29f098735abe3621b7d9b668e00cfab
Phychias <51688899+Phychias@users.noreply.github.com>
2025-04-22 17:57:40 +0200

    LAPTOP-UGNPREPA 2025-4-22:17:57:40

--- docs/assets/js/map.js
+++ docs/assets/js/map.js
@@ -9,23 +9,38 @@ document.addEventListener("DOMContentLoaded", function () {
   var w = 4800, h = 2700;
   var bounds = [[0,0], [h,w]];
 
-  // 添加多图层（生态图、疆域图）
+  // ------------------------添加多图层（生态图、疆域图）------------------------------------------------------
   // var ecoLayer = L.imageOverlay('img/Trusk/Trusk v3.2 生态 中文.png', bounds);
   // var regionLayer = L.imageOverlay('img/Trusk/图斯克疆域图v2.png', bounds);
   var baseLayer = L.imageOverlay('../../assets/img/trusk_render.png', bounds);
 
+  //疆域图
+  var regionBounds = [[2200, 1884.00000], [200, 3400]];
+  var regionLayer = L.imageOverlay('../../assets/img/region.png', regionBounds);
+
   baseLayer.addTo(map);
   map.fitBounds(bounds);
 
-  // 添加图层切换
+  // -------------------------------添加图层切换--------------------------
   var baseMaps = {
-    "基础图": baseLayer,
-    //"生态图": ecoLayer,
-    //"疆域图": regionLayer
+    "地形": baseLayer,
   };
-  L.control.layers(baseMaps).addTo(map);
 
-  // 添加标记
-  L.marker([500, 1200]).bindPopup("图斯克都城").addTo(map);
-  L.marker([300, 200]).bindPopup("边境哨所").addTo(map);
+  var overlayMaps = {
+    //"生态图": ecoLayer,
+    "疆域图": regionLayer
+  };
+
+  L.control.layers(baseMaps, overlayMaps).addTo(map);
+  
+  // 监听地图点击事件
+  map.on('click', function(e) {
+    var latlng = e.latlng; // 获取点击的坐标
+
+    // 创建一个标记并添加到地图
+    L.marker(latlng).addTo(map)
+        .bindPopup("坐标: " + latlng.lat.toFixed(5) + ", " + latlng.lng.toFixed(5))
+        .openPopup();
+  });
+
 });
