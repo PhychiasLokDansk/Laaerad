1f973f7187cc696f3d31e4021e3716af746a8b7a
Phychias <51688899+Phychias@users.noreply.github.com>
2025-04-22 14:40:19 +0200

    LAPTOP-UGNPREPA 2025-4-22:14:40:18

--- docs/assets/js/map.js
+++ docs/assets/js/map.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // 添加多图层（生态图、疆域图）
   // var ecoLayer = L.imageOverlay('img/Trusk/Trusk v3.2 生态 中文.png', bounds);
   // var regionLayer = L.imageOverlay('img/Trusk/图斯克疆域图v2.png', bounds);
-  var baseLayer = L.imageOverlay('../../Laaerad/Trusk/trusk_render.png', bounds);
+  var baseLayer = L.imageOverlay('img/Trusk/trusk_render.png', bounds);
 
   baseLayer.addTo(map);
   map.fitBounds(bounds);
