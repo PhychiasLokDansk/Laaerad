1b6dd76e84f6bb63a06df3e7ed20eaeaed4bdc3c
Phychias <51688899+Phychias@users.noreply.github.com>
2025-04-22 13:17:34 +0200

    LAPTOP-UGNPREPA 2025-4-22:13:17:34

--- docs/assets/js/map.js
+++ docs/assets/js/map.js
@@ -1,3 +1,9 @@
+<><link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+  integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+  crossorigin="" /><script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+    crossorigin=""></script></>
+
 document.addEventListener("DOMContentLoaded", function () {
   // 定义简单坐标系地图，像素坐标，0,0在左上角
   var map = L.map('map', {
