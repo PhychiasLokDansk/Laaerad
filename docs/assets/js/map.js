dc357ed002fea8f99b0b1532164767b7a4caf4a9
Phychias <51688899+Phychias@users.noreply.github.com>
2025-04-26 20:03:21 +0200

    LAPTOP-UGNPREPA 2025-4-26:20:3:21

--- docs/assets/js/map.js
+++ docs/assets/js/map.js
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", function () {
   */
 
   // 添加标记
-  L.marker([1000.59896, 2775.75173]).bindPopup("<br><a href='../../../Laaerad/Trusk/Trusk' target='_blank'>图斯克介绍</a>").addTo(map);
+  L.marker([1000.59896, 2775.75173]).bindPopup("<br><a href='../../../Laaerad/Trusk/Trusk/' target='_blank'>图斯克介绍</a>").addTo(map);
 
 
 });
